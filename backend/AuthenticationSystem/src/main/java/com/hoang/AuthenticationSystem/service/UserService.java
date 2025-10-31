b3e10d5a32aea9458c2b02e82bb5b465373dbe91
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2025-10-31 17:58:47 +0700

    Add UserService

---
+++ backend/AuthenticationSystem/src/main/java/com/hoang/AuthenticationSystem/service/UserService.java
@@ -0,0 +1,26 @@
+package com.hoang.AuthenticationSystem.service;
+
+import com.hoang.AuthenticationSystem.model.User;
+import com.hoang.AuthenticationSystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+}
