c1b403a90bb474a1774da58c8513109a17c13786
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2025-10-31 17:57:02 +0700

    Add entity User.java

---
+++ backend/AuthenticationSystem/src/main/java/com/hoang/AuthenticationSystem/model/User.java
@@ -0,0 +1,66 @@
+package com.hoang.AuthenticationSystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(nullable = false, unique = true)
+    String username;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String firstname;
+
+    @Column(nullable = false)
+    String lastname;
+
+    String avatarUrl;
+
+    String avatarId; // Cloudinary public ID
+
+    @Builder.Default
+    boolean enabled = false;
+
+    @Column(name = "verification_code")
+    String verificationCode;
+
+    @Column(name = "verification_expiration")
+    LocalDateTime verificationExpiresAt;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}
